795552af124bef884bbb6c4a306491fabdb07c05
siman4457 <siman4457@gmail.com>
2021-02-11 23:56:35 -0600

    fixed /findAvailableEmployees

--- routes/schedules.js
+++ routes/schedules.js
@@ -79,112 +79,112 @@ function isOverlapping(dateRanges){
 }
 
 async function isAvailableAtTime(employee, start, end){
-    let available = true;
+    console.log("isAvailableAtTime Fired");
+    try{
+        let available = true;
+        await getGames(employee.games)
+        .then(res =>{
+            res.forEach(game => {
+                // console.log(game.title + " starts at " + game.datetime)
+                let cur_game_start = new Date(game.datetime);
+                let cur_game_end = new Date(game.datetime);
+                cur_game_end.setHours( cur_game_end.getHours() + 2 ); //TODO: CHANGE THIS TO GET THE ACTUAL END TIME. CURRENTLY JUST ADDING 2 HOURS
+                
+                let new_game_start = new Date(start);
+                let new_game_end = new Date(end);
+                new_game_end.setHours( new_game_end.getHours() + 2 ); //TODO: CHANGE THIS TO GET THE ACTUAL END TIME. CURRENTLY JUST ADDING 2 HOURS
+                
+                let cur_game_times = {
+                    start: cur_game_start,
+                    end: cur_game_end
+                };
+                let new_game_times = {
+                    start: new_game_start,
+                    end: new_game_end
+                };
+                
+                let dateRanges = [cur_game_times, new_game_times];
 
-    await getGames(employee.games)
-    .then(res =>{
-        res.forEach(game => {
-            // console.log(game.title + " starts at " + game.datetime)
-            let cur_game_start = new Date(game.datetime);
-            let cur_game_end = new Date(game.datetime);
-            cur_game_end.setHours( cur_game_end.getHours() + 2 ); //TODO: CHANGE THIS TO GET THE ACTUAL END TIME. CURRENTLY JUST ADDING 2 HOURS
-            
-            let new_game_start = new Date(start);
-            let new_game_end = new Date(end);
-            new_game_end.setHours( new_game_end.getHours() + 2 ); //TODO: CHANGE THIS TO GET THE ACTUAL END TIME. CURRENTLY JUST ADDING 2 HOURS
-            
-            let cur_game_times = {
-                start: cur_game_start,
-                end: cur_game_end
-            };
-            let new_game_times = {
-                start: new_game_start,
-                end: new_game_end
-            };
-            
-            let dateRanges = [cur_game_times, new_game_times];
+                if(isOverlapping(dateRanges)){
+                    available = false;
+                }
 
-            if(isOverlapping(dateRanges)){
-                available = false;
-            }
-
+            });
+        })
+        .catch(err =>{
+            console.error("Error in isAvailableAtTime: ", err);
         });
-    })
-    .catch(err =>{
-        console.error("Error in isAvailableAtTime: ", err);
-    });
-
-    return available;
+        console.log("isAvailableAtTime Finished - available: ", available);
+        return available;
+    }
+    catch(error){
+        console.log("Error in isAvailableAtTime: ", error);
+    }
 }
 
 async function isAvailable(employee, start, end){
+        let day = isAvailableOnDay(employee, start);
+        // let time = await isAvailableAtTime(employee, start, end);
 
-    let day = isAvailableOnDay(employee, start);
-    let time = await isAvailableAtTime(employee, start, end);
-    console.log({day, time});
+        let availability = await isAvailableAtTime(employee, start, end)
+        .then(hasTime => {
+            if( day && hasTime){
+                // If the employee is available on the given day, check to see if the new game's location 
+                // is different from the employee's current location. Then use Google Maps API to 
+                // determine if the employee has time to drive there and get set up.
+                
+                return employee;
+            }
+        })
+        .catch(error => {
+            console.log("Error in isAvailable: ", error);
+        });
 
-    if( day && time){
-        // If the employee is available on the given day, check to see if the new game's location 
-        // is different from the employee's current location. Then use Google Maps API to 
-        // determine if the employee has time to drive there and get set up.
-        return true;
-    }
-    else{        
-        return false;
-    }
+        return availability;
 }
 
-
-
-router.get('/findAvailableEmployees', async(req, res) => {
-    const availableEmployees = [];
+router.post('/findAvailableEmployees', async(req, res) => {
+    let availableEmployees = [];
+    const promises = [];
     const start = req.body.start;
     const end = req.body.end;
+
     try{
-        await Employee.find({}, function(err, employees){
-            employees.forEach(employee => {
-                // if(isAvailable(employee, start, end)){
-                    
-                //     //TODO: Check if game has already been assigned for the given time slot.
-                //     // Also check if employee has enough time to set up or travel to the given location
-                //     // if the location is not the same
+        let employees = await Employee.find({});
 
-                //     // employee.games.forEach(game => {
-                //     //     console.log('game datetime: ', game.datetime)
-                //     // })
-                //     console.log("pushing ", employee['first_name']);
-                //     availableEmployees.push(employee);
-                // }
-                isAvailable(employee, start, end)
-                .then(res => {
-                    // console.log({res});
-                    if(res === true){
-                        console.log("pushing ", employee['first_name']);
-                        availableEmployees.push(employee);
-                    }
-                })
-                .catch(err => {
-                    console.log("Error in isAvailable: ", err);
-                });
+        employees.forEach(employee => {
+            //     //TODO: Check if game has already been assigned for the given time slot.
+            //     // Also check if employee has enough time to set up or travel to the given location
+            //     // if the location is not the same
+
+            promises.push(isAvailable(employee, start, end));
+        });
+        
+
+        Promise.all(promises)
+        .then((results)=>{
+            availableEmployees = results.filter(x => x !== undefined);
+            
+
+            if(availableEmployees.length < 1){
+                console.log("No available employees");
+                let none = {
+                    _id: 0,
+                    first_name: "None"
+                };
+                availableEmployees.push(none);
+            } 
+    
+            res.status(200).json({
+                message: "Request to /findAvailableEmployees was successful.",
+                availableEmployees: availableEmployees
             });
+        })
+        .catch(error =>{
+            console.log("Error in /findAvailableEmployees", error);
         });
-
-        if(availableEmployees.length < 1){
-            console.log("No available employees");
-            let none = {
-                _id: 0,
-                first_name: "None"
-            };
-            availableEmployees.push(none);
-        } 
-
-        console.log("Function fired.");
-        res.status(200).json({
-            message: "Request to /findAvailableEmployees was successful.",
-            availableEmployees: availableEmployees
-        });
-
     }
+
     catch(error){
         console.log("Error in /findAvailableEmployees", error);
         res.status(424).json({
